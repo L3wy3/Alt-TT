35ac50092e789be28ae01d36a314004f4f8a90e7
Laurence Towning <lstowning@outlook.com>
2025-09-22 10:10:54 +0100

    unsaved ominous file

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -4,7 +4,7 @@
         <div class="w-full flex text-[#FFF] items-center flex-col lg:flex-row ml-auto mr-auto h-full justify-around max-w-[80%] md:max-w-[100%]">
             <div class="w-full">
                 <h2 class="max-w-[500px] leading-none mb-[14px] text-[24px] md:text-[40px] lg:text-[70px] font-bold" style="font-family: 'Gilroy-Medium', sans-serif;">We make your IT work easy.</h2>
-                <p class="font-normal font-(family-name:IBMvPlexMono) text-base leading-[30px] max-w-[570px]"style="font-family: 'IBM Plex Mono', monospace;">Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them. </p>
+                <p class="font-normal font-(family-name:IBMvPlexMono) text-base leading-[30px] max-w-[570px]" style="font-family: 'IBM Plex Mono', monospace;">Recognising your frustrations with your print environment, IT services, document management & communications and finding a solution to overcome them. </p>
             </div>
             <div class="w-full p-r-[32px] max-h-[300px] max-w-[80%]">
                 <div class="max-w-[480px] relative ml-auto mr-auto mt-[-90px] md:mt-[0px]">
